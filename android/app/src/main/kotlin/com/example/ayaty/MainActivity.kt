8731da9b0d40afdc6cdfb1ab6dab8976cb99b981
Melmohands22 <melmohands591@gmail.com>
2025-03-07 15:31:51 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/ayaty/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ayaty
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Ayaty());
+}
+
+class Ayaty extends StatelessWidget {
+  const Ayaty({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
